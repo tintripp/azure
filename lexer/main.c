7bd451c286c0527455354f72cd6f6d2b7e1cbf23
Timothy Tripp <ti.n.tripp@baypath.net>
2026-01-20 11:32:42 -0500

    Lexer_Init Beta

--- lexer/main.c
+++ lexer/main.c
@@ -1,42 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-char *OpenFile(char *name){
-    FILE *f = fopen(name, "r");
-    if (!f) return NULL;
+typedef enum  {
+    IDENTIFIER,
+    KEYWORD,
+    SEPARATOR,
+    OPERATOR,
+    LITERAL,
+    // COMMENT, this will be ignored
+} TokenType;
 
-    if (fseek(f, 0, SEEK_END) != 0) {
-        fclose(f);
-        return NULL;
-    }
+typedef struct {
+    TokenType type;
+    char *lexeme;
+    // optionally, location info for error messages
+} Token;
+
+typedef struct {
+    const char *source;
+    size_t length;
+    size_t pos;
+    int line;
+    int column;
+} Lexer;
+
+void Lexer_Init(Lexer *lexer, const char *source, int length){
+    lexer->source = source;
+    lexer->length = length;
+
+}
+
+bool Lexer_NextToken(Lexer *lexer, Token *out){
+    printf("NextToken");
+}
+
+size_t Lexer_FileSize(FILE *f){
+    if (fseek(f, 0, SEEK_END) != 0)
+        return (size_t)-1;
+    
     long size = ftell(f);
-    if (size < 0) {
-        fclose(f);
-        return NULL;
-    }
+    if (size < 0)
+        return (size_t)-1;
+    
     rewind(f);
 
-    char *str = malloc((size_t)size + 1); // + 1 for nullterm
+    return (size_t)size;
+}
+
+char *Lexer_ReadFile(FILE *f){
+
+    char *str = malloc(Lexer_FileSize(f) + 1); // + 1 for nullterm
     int c, i = 0;
     while((c = fgetc(f)) != EOF) {
         str[i++] = c;
     }
 
     str[i] = '\0';
-    fclose(f); 
 
     return str;
 }
 
 int main(int argc, char *argv[]) {
-    printf("Hello world!\n%s %s\n",argv[0],argv[1]);
+    printf("Azure Lexer\ninput: %s\n",argv[1]);
 
-    char *result;
-    if (argc > 1)
-        result = OpenFile(argv[1]);
+    Lexer *lexer;
+    if (argc > 1) {
 
-    if (result)
-        printf("%s\n", result);
+        FILE *f = fopen(argv[1], "r");
+        if (!f) return NULL;
+
+        Lexer_Init(lexer, Lexer_ReadFile(f), Lexer_FileSize(f));
+
+        fclose(f);
+    }
+
+    if (lexer)
+        printf("%s\n", lexer->source);
     else
         printf("Null\n");
 
